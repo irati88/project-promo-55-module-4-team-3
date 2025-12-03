ed3b52f8e99ff32a1e0d598e93c0e238d9d3d44f
irati88 <irati.a88@gmail.com>
2025-12-03 18:35:41 +0100

    Intento 2 deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,13 +15,13 @@ const port = 3000;
 app.use(express.json({ limit: "25mb" }));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "..", "public")));
-
 app.get("/projects", getProjectsController);
 
 app.get("/project/:id", getProjectIdController);
 
 app.post("/project", postProjectController);
+
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use((req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/project-promo-55-module-3-team-3/",
+  base: "/project-promo-55-module-4-team-3/",
   server: {
     open: "/",
     watch: {
