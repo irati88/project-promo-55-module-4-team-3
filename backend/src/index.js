5d386ef6d648a442346f632efc3e2be7263d0478
irati88 <irati.a88@gmail.com>
2025-12-03 18:23:18 +0100

    Trying to deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,32 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
+require("dotenv").config();
+
 const {
   getProjectsController,
   getProjectIdController,
   postProjectController,
 } = require("./controllers");
+
 const app = express();
 const port = 3000;
 
-require("dotenv").config();
-
 app.use(express.json({ limit: "25mb" }));
-
 app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/projects", getProjectsController);
 
 app.get("/project/:id", getProjectIdController);
 
 app.post("/project", postProjectController);
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
